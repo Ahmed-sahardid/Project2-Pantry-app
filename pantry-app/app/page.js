8ac2a6bd969bbe6a8246e851dfe9689d885875c4
Ahmed Sahardid <sahar021@umn.edu>
2024-07-29 17:04:41 -0500

    h

--- pantry-app/app/page.js
+++ pantry-app/app/page.js
@@ -1,14 +1,35 @@
-import React from 'react';
 import Box from '@mui/material/Box';
-export default function Home() {
-  return (
-  <Box 
-    width="100vw"
-    height="100vh"
-    display = {"flex"}
-    justifyContent={"center"}
-    alignItem={"center"} 
-    >
-    Hello world</Box>
-  );
+import Stack from '@mui/material/Box';
+
+
+export default function Home(){
+  const item = [
+    "tomato",
+    "potato",
+    "onion",
+    "garlic",
+    "ginger"
+  ]  
+
+  return <Box 
+    width={"100vw"} 
+    height={"100vh"} 
+    display={"flex"}
+    alignItems={"center"} 
+    justifyContent={"center"}>
+
+    <Stack width={"800px"} height={"600px"} Spacing={2}>
+
+      {item.map(([i]) =>(
+      <Box key={i}
+      width={"100vw"} 
+      height={"100vh"} 
+      display={"flex"}
+      alignItems={"center"} 
+      justifyContent={"center"}>
+        
+    </Box>
+      ))}
+    </Stack>
+  </Box>
 }
