980057d1477c440286c6bcbebb9e6d1537315d61
Ahmed Sahardid <sahar021@umn.edu>
2024-07-29 22:38:25 -0500

    hello

--- pantry-app/app/page.js
+++ pantry-app/app/page.js
@@ -1,35 +1,27 @@
-import Box from '@mui/material/Box';
-import Stack from '@mui/material/Box';
+import React from 'react';
+import { Box, Stack } from '@mui/material';
 
+export default function Page() {
+  const items = ["potato","cabbage","garlic","ginger","carrot"];
 
-export default function Home(){
-  const item = [
-    "tomato",
-    "potato",
-    "onion",
-    "garlic",
-    "ginger"
-  ]  
-
-  return <Box 
-    width={"100vw"} 
-    height={"100vh"} 
-    display={"flex"}
-    alignItems={"center"} 
-    justifyContent={"center"}>
-
-    <Stack width={"800px"} height={"600px"} Spacing={2}>
-
-      {item.map(([i]) =>(
-      <Box key={i}
-      width={"100vw"} 
-      height={"100vh"} 
-      display={"flex"}
-      alignItems={"center"} 
-      justifyContent={"center"}>
-        
+  return (
+    <Box width="100vw" height="100vh">
+      <Stack width="800px" height="600px" spacing={2} alignItems="center" justifyContent="center">
+        {items.map((i) => (
+          <Box key={i} 
+          width="100vw" 
+          height="100vh"
+          display={"flex"}
+          justifyContent={"center"}
+          alignItems={"center"}
+          bgcolor={"f0f0f0"}
+          >
+            {
+              i
+            }
+          </Box>
+        ))}
+      </Stack>
     </Box>
-      ))}
-    </Stack>
-  </Box>
+  );
 }
